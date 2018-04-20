5a691853d464e36d806eadb6f8239174b7d3c3a7
Denys <desmond745@gmail.com>
2018-04-20 12:18:07 +0300

    Добавил pug и доделал главную страницу

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,8 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass');
 const browserSync = require('browser-sync');
-
-// gulp.task('hello', function() {
-//     console.log('Hello Zell');
-// });
-
-// gulp.task('sass', function(){
-//     return gulp.src('app/scss/**/*.scss')
-//     .pipe(sass()) // используем gulp-sass
-//     .pipe(gulp.dest('app/css'))
-// });
-    
+const pug = require('gulp-pug');
+   
 gulp.task('sass', function () {
     return gulp.src('app/scss/**/*.scss')
       .pipe(sass().on('error', sass.logError))
@@ -20,11 +11,21 @@ gulp.task('sass', function () {
         stream: true 
        }))
   });
- 
-gulp.task('watch', ['browserSync', 'sass'], function(){
+
+gulp.task('pug',function() {
+    return gulp.src('app/*.pug')
+    .pipe(pug({
+       doctype: 'html',
+       pretty: false
+    }))
+  .pipe(gulp.dest('app/'));
+  }); 
+
+gulp.task('watch', ['browserSync', 'sass', 'pug'], function(){
     gulp.watch('app/scss/**/*.scss', ['sass']);
     gulp.watch('app/*.html', browserSync.reload);
-    gulp.watch('app/js/**/*.js', browserSync.reload)  
+    gulp.watch('app/js/**/*.js', browserSync.reload);
+    gulp.watch('app/**/*.pug', ['pug'], browserSync);  
 })
 
 gulp.task('browserSync', function() {
